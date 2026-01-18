4c0cbc200196e9d451d2614d2aa37b8eb9834329
euefrai <bsdfai1001@gmail.com>
2026-01-18 11:15:38 -0300

    Update clientes.py

--- routes/clientes.py
+++ routes/clientes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, redirect, session, url_for
-from db import get_db
+from utils.db import get_db
 
 clientes_bp = Blueprint("clientes", __name__)
 
